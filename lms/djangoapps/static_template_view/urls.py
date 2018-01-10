c3365c3ac7e3156be778cc521ff8e9b03dff5193
risualSupport <30312631+risualSupport@users.noreply.github.com>
2018-01-10 15:21:47 +0000

    added about url

--- lms/djangoapps/static_template_view/urls.py
+++ lms/djangoapps/static_template_view/urls.py
@@ -16,7 +16,8 @@ urlpatterns = (
     # display error page templates, for testing purposes
     url(r'^404$', 'render_404'),  # Can this be deleted? Test test_404_microsites fails with this.
     url(r'^500$', 'render_500'),
-
+    
+    url(r'^blog$', 'render', {'template': 'about.html'}, name="about"
     url(r'^blog$', 'render', {'template': 'blog.html'}, name="blog"),
     url(r'^contact$', 'render', {'template': 'contact.html'}, name="contact"),
     url(r'^donate$', 'render', {'template': 'donate.html'}, name="donate"),
