18da1c20ace218785b30f94ce6ecb4ffe92675fd
risualSupport <support@risual.com>
2018-02-14 15:20:58 +0000

    theme update

--- lms/djangoapps/static_template_view/urls.py
+++ lms/djangoapps/static_template_view/urls.py
@@ -17,6 +17,7 @@ urlpatterns = (
     url(r'^404$', 'render_404'),  # Can this be deleted? Test test_404_microsites fails with this.
     url(r'^500$', 'render_500'), 
     
+	url(r'^contact-form$', 'render', {'template': 'contactform.py'}, name="contact-form"),
     url(r'^cookie-policy$', 'render', {'template': 'cookie-policy.html'}, name="cookie-policy"),
     url(r'^cookie-policy$', 'render', {'template': 'cookie-policy.html'}, name="cookie-policy"),
     url(r'^privacy$', 'render', {'template': 'privacy.html'}, name="privacy"),
